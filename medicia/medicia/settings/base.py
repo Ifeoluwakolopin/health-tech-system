99e2846fb7cae46d607958e73dcdecfb1638e870
Ifeoluwakolopin <areifeoluwa10@gmail.com>
2021-04-28 20:31:54 +0100

    added new app "account and changed auth_user_model

--- medicia/medicia/settings/base.py
+++ medicia/medicia/settings/base.py
@@ -28,7 +28,7 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
-AUTH_USER_MODEL = "records.User"
+AUTH_USER_MODEL = "account.User"
 
 # Application definition
 
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'account',
     'records',
 ]
 
