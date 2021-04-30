d7db45380c6f8ae00f928a4c230f78187187aa92
Ifeoluwakolopin <areifeoluwa10@gmail.com>
2021-04-30 21:26:09 +0100

    removed namespace and added password change views

--- medicia/medicia/urls.py
+++ medicia/medicia/urls.py
@@ -20,6 +20,6 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path("", include("records.urls", namespace="records")),
-    path("account/", include("account.urls", namespace="account")),
+    path("", include("records.urls")),
+    path("account/", include("account.urls")),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
